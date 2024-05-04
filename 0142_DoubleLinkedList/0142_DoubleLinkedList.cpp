73de3eaf0e05841aeb1e7296d65174733c7661d1
Galang Sakti G <wadepakmen97@gmail.com>
2024-05-04 23:10:08 +0700

    Memasukkan Void Delete

--- 0142_DoubleLinkedList/0142_DoubleLinkedList.cpp
+++ 0142_DoubleLinkedList/0142_DoubleLinkedList.cpp
@@ -77,6 +77,13 @@ bool search(int rollNo, Node** previous, Node** current)
 	return (*current != NULL);
 }
 
+void deleteNode()
+{
+	Node* previous, * current;
+	int rollNo;
+
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo;
 
 
 
